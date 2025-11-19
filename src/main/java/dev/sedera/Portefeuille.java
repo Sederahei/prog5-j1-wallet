3137057a01f4bf4c49c0522ab8dc2d2948c63a19
sederahei <hei.sedera.167@gmail.com>
2025-11-19 14:12:20 +0300

    feat: add protfeuille class

--- src/main/java/dev/sedera/Portefeuille.java
+++ src/main/java/dev/sedera/Portefeuille.java
@@ -1,4 +1,66 @@
 package dev.sedera;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Portefeuille {
+    private String couleur;
+    private int porteCarte;
+    private double poids;
+    private String marque;
+    private double argent;
+    private List<String> cartes;
+
+    public Portefeuille(String couleur, int porteCarte, double poids, String marque) {
+        this.couleur = couleur;
+        this.porteCarte = porteCarte;
+        this.poids = poids;
+        this.marque = marque;
+        this.argent = 0.0;
+        this.cartes = new ArrayList<>();
+    }
+
+    public void addMoney(double montant) {
+        if (montant > 0) {
+            argent += montant;
+        }
+    }
+
+    public boolean getMoney(double montant) {
+        if (montant > 0 && montant <= argent) {
+            argent -= montant;
+            return true;
+        }
+        return false;
+    }
+
+    public double checkMoney() {
+        return argent;
+    }
+
+    public boolean addCard(String carte) {
+        if (cartes.size() < porteCarte) {
+            cartes.add(carte);
+            return true;
+        }
+        return false;
+    }
+
+    public void lost() {
+        argent = 0.0;
+        cartes.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Portefeuille{" +
+                "couleur='" + couleur + '\'' +
+                ", porteCarte=" + porteCarte +
+                ", poids=" + poids +
+                ", marque='" + marque + '\'' +
+                ", argent=" + argent +
+                ", cartes=" + cartes +
+                '}';
+    }
 }
+
